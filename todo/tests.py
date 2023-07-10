d0c9b013c9603a5b3fca3337965f70e41101bb3e
s1f10220136 <s1f102201368@iniad.org>
2023-07-10 17:07:41 +0900

    ソース機能を追加

--- todo/tests.py
+++ todo/tests.py
@@ -33,7 +33,7 @@ class TaskModelTestCase(TestCase):
 
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
-        current = timezone.make_aware(datetime(2023,6, 30, 0, 0, ,0))
+        current = timezone.make_aware(datetime(2023,6, 30, 0, 0,0))
         task = Task(title='task1', due_at=due)
         task.save()
 
@@ -44,10 +44,44 @@ class TaskModelTestCase(TestCase):
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
-        date = {'title' : 'Test Task', 'due_at': '2023-06-30 23:59:59'}
-        respomse =client.get('/')
+        response =client.get('/')
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(resuponse.context['tasks']), 1)
+        self.assertEqual(len(response.context['tasks']), 1)
 
+
+    def test_insex_post(self):
+        client= Client()
+        data = {'title' : 'Test Task', 'due_at': '2023-06-30 23:59:59'}
+        response = client.post('/',data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(len(response.context['tasks']), 1)
+
+    def test_index_get_order_post(self):
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2023, 7, 1)))
+        task1.save()
+        task2 =Task(title ='task2', due_at=timezone.make_aware(datetime(2023, 8 ,1)))
+        task2.save()
+        client =Client()
+        response =client.get('/?order=post')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(response.context['tasks'][0],task2)
+        self.assertEqual(response.context['tasks'][1],task1)
+
+    def test_index_get_order_due(self):
+        task1 =Task(title='task1', due_at= timezone.make_aware(datetime(2023, 7, 1)))
+        task1.save()
+        task2 =Task(title ='task2', due_at=timezone.make_aware(datetime(2023, 8 ,1)))
+        task2.save()
+        client =Client()
+        response =client.get('/?order=post')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(response.context['tasks'][0],task1)
+        self.assertEqual(response.context['tasks'][1],task2)
