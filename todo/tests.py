4f43d96d0649a44ed53f74bbcac511544cf013ab
s1f10220136 <s1f102201368@iniad.org>
2023-07-13 09:21:32 +0900

    修正

--- todo/tests.py
+++ todo/tests.py
@@ -47,10 +47,8 @@ class TodoViewTestCase(TestCase):
         response =client.get('/')
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']), 0)
-
-
+        self.assertEqual(response.templates[0].name,'todo/index.html')
+        self.assertEqual(len(response.context['tasks']),0)
     def test_insex_post(self):
         client= Client()
         data = {'title' : 'Test Task', 'due_at': '2023-06-30 23:59:59'}
@@ -58,12 +56,11 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']), 1)
-
+        self.assertEqual(len(response.context['tasks']),1)
     def test_index_get_order_post(self):
-        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2023, 7, 1)))
+        task1 = Task(title='task1',due_at=timezone.make_aware(datetime(2023,7,1)))
         task1.save()
-        task2 =Task(title ='task2', due_at=timezone.make_aware(datetime(2023, 8 ,1)))
+        task2 =Task(title ='task2',due_at=timezone.make_aware(datetime(2023,8,1)))
         task2.save()
         client =Client()
         response =client.get('/?order=post')
@@ -74,14 +71,14 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][1],task1)
 
     def test_index_get_order_due(self):
-        task1 =Task(title='task1', due_at= timezone.make_aware(datetime(2023, 7, 1)))
+        task1 =Task(title='task1',due_at= timezone.make_aware(datetime(2023,7,1)))
         task1.save()
-        task2 =Task(title ='task2', due_at=timezone.make_aware(datetime(2023, 8 ,1)))
+        task2 =Task(title ='task2',due_at=timezone.make_aware(datetime(2023,8,1)))
         task2.save()
         client =Client()
         response =client.get('/?order=due')
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(response.templates[0].name,'todo/index.html')
         self.assertEqual(response.context['tasks'][0],task1)
         self.assertEqual(response.context['tasks'][1],task2)
